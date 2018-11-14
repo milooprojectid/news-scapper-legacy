b44aa2894e5fb2c25ad11707c043d2feb8123016
fuad_i_amal <fuad_i_amal@telkomsel.co.id>
2018-11-14 22:23:58 +0700

    merge mongo connection code and news crawler code into a single code

--- sandbox/news_crawler.py
+++ sandbox/news_crawler.py
@@ -15,30 +15,48 @@
 
 from __future__ import division
 import re
+import pymongo
 import requests
 import warnings
 from bs4 import BeautifulSoup
 
 
 
-# define GLOBAL STATIC VARIABLE
-NEWS_SOURCE_URL = "https://www.detik.com"
-NEWS_DOMAIN_URL = "detik.com"
-VALID_LINK_URL_RE = re.compile(r"^https?://\S+\." + NEWS_DOMAIN_URL + "/?\S*$")
+warnings.filterwarnings("ignore")
+
+
+
+def connect_mongodb():
+	return pymongo.MongoClient("mongodb://sakoju:E107112358@178.128.98.252:2017/milo-staging?authSource=admin")
+
+
+
+def crawl(source_name, url, target_url):
+	req_ = requests.get(target_url)
+	html_ = req_.text
+	soup_ = BeautifulSoup(html_, "html.parser")
+	valid_url_re = re.compile(r"^https?://\S*" + url + "/?\S*$")
+	
+
+	# specify the mongodb connection, database and collection
+	db_cnx = connect_mongodb()
+	db_ = db_cnx["milo-staging"]
+	link_collection = db_.links
+
+
+	for alink in soup_.findAll("a", href=True):
+		href_ = str(alink["href"]).strip().lower()
+		if valid_url_re.match(href_):
+			link_collection.insert_one({
+				"url": href_, "source": source_name,
+				"status": 
+			})
+			# save link here, one-by-one inside loop
+
+
+	# bulk save here, list of links...
 
 
 
 if __name__ == '__main__':
-	warnings.filterwarnings("ignore")
-
-	
-	req_ = requests.get(NEWS_SOURCE_URL)
-	html_ = req_.text
-	soup_ = BeautifulSoup(html_, "html.parser")
-	valid_url_counter = 0
-
-
-	for alink in soup_.findAll("a", href=True):
-		href_ = str(alink["href"])
-		if VALID_LINK_URL_RE.match(href_):
-			print(href_)
+	crawl("detik", "detik.com", "https://www.detik.com")
