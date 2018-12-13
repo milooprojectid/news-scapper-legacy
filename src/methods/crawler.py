ba1b2099ee1beb82e95a642e90054bce5e9c89fc
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-12-13 17:56:46 +0700

    merge code manually

--- src/methods/crawler.py
+++ src/methods/crawler.py
@@ -1,85 +1,102 @@
 # Author: Fuad Ikhlasul Amal
 # Created date: 07-11-2018
-# Updated date: 28-11-2018
+# Updated date: 05-12-2018
 # Version: 1.2
-# Function: 
+# Function:
 #	* perform simple grab html content from http/https web page
 # 	* find any url that refer to the self root domain url
-#	* collect the urls and save to database
-#	* add Flask wrapper
-#	* add extract, pre-process and save DOM elements function
-# Run code: python crawler.py
+#	* collect the urls and grab the HTML page DOMs, save to mongodb
+#	* add Flask wrapper and provide API end-point
+#	* add regexp to classify valid-news-url and non-valid-news-url
+#	* add extract and preprocess HTML DOM elements including:
+#	*		- remove <script>, <style> and <link> tags
+#	* 		- remove inline style="" attribute from any html tag
+# 	*		- remove html comments
+# 	*		- minified DOM HTML
+# Run code: python news_crawler.py
+# curl POST -X https://api_host_name_or_ip:api_port_number/crawl {"source": "...", "url": "...", "url_target": "..."}
+
 
 from __future__ import division
+import re, os, string, htmlmin, requests, warnings
 from bs4 import BeautifulSoup, Comment
 import src.utils.mongodb as mongo
-import re, sys, requests, warnings
 
 warnings.filterwarnings("ignore")
+_BASEDIR = os.path.dirname(os.path.realpath(__file__))
+
 
 def do_crawl(source_name, url, target_url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'}
     req_ = requests.get(target_url, headers=headers)
-    html_ = str(req_.text)
+    html_ = req_.text
     soup_ = BeautifulSoup(html_, "html.parser")
     valid_url_re = re.compile(r"^https?://\S*" + url + "/?\S*$")
 
-    # specify the mongodb connection, database and collection
     db_ = mongo.getInstance()
     link_collection = db_.links
+    dom_collection = db_.raws
 
-    # remove <script>, <style> and <link>
-    soup_raw = soup_
     for tag in soup_.findAll():
         if tag.name.lower() in ("script", "style", "link"):
             tag.extract()
-    # remove inline style in html tag
-    # for attr
+        del tag["style"]
 
-    # remove comments block
     for comment in soup_.findAll(text=lambda text: isinstance(text, Comment)):
         comment.extract()
 
-    fhtml_raw = open("html_dom_raw_sample.out", "w")
-    sys.stdout = fhtml_raw
-    print(html_)
-    sys.stdout = sys.__stdout__
-    fhtml_raw.close()
+    html_dom_clean = "".join(filter(lambda _: _ in set(string.printable), str(soup_)))
+    html_dom_clean = htmlmin.minify(html_dom_clean, remove_comments=True, remove_empty_space=True,
+                                    remove_all_empty_space=True)
 
-    fhtml_clean = open("html_dom_clean_sample.out", "w")
-    sys.stdout = fhtml_clean
-    print(soup_)
-    sys.stdout = sys.__stdout__
-    fhtml_clean.close()
+    target_url = target_url.split("?")[0]
 
-    # quit()
+    if "detik" == source_name:
+        news_url_re = re.compile(r"^(https://(www\.)?(\S+\.)?detik\.com/[a-z\-]+/\w*\-\d+/[a-z0-9\-]+)$")
+    elif "kompas" == source_name:
+        pass
+    elif "kumparan" == source_name:
+        pass
+    else:
+        pass
 
-    # init the mongo bulk object to upsert (update-or-insert) the link data
+    if news_url_re.match(target_url):
+        data_ = {
+            "source": source_name,
+            "url": target_url,
+            "content": html_dom_clean,
+            "status": 0,
+            "visited_at": None
+        }
+        dom_collection.update({"content": html_dom_clean, "source": source_name}, data_, upsert=True)
+        print("insert dom success !!")
+
     link_bulk = link_collection.initialize_ordered_bulk_op()
 
     for alink in soup_.findAll("a", href=True):
         href_ = str(alink["href"]).strip().lower()
-
-        # remove params or arguments from url after '?'
         href_ = href_.split("?")[0]
 
         if valid_url_re.match(href_):
             url_ = {"url": href_, "source": source_name}
-            # save link here, one-by-one inside loop
             link_bulk.find(url_).upsert().update({
                 "$setOnInsert": {"status": 0, "visited_at": None},
                 "$set": url_
             })
 
-    # bulk save here, list of links...
     try:
         bulkop_resp = link_bulk.execute()
+        print("insert bulk links success !!")
         return True
-    except Exception:
+    except Exception as  e:
         return None
 
 
+
 if __name__ == '__main__':
+    # this is the actual way to call crawl service
     # app.run(debug=True)
-    do_crawl("viva", "viva.co.id", "https://www.viva.co.id/")
+
+    # code below was provided for testing purpose
+    do_crawl("detik", "detik.com", "https://hot.detik.com/kpop/d-4340712/heboh-iklan-blackpink-dinilai-seronok-shopee-dipanggil-kpai")
---
+++ src/methods/scrapper.py
@@ -0,0 +1,143 @@
+# !/usr/bin/env python
+# -*-coding: utf-8 -*-
+# system module
+
+from __future__ import division
+import re
+import requests
+import warnings
+from bs4 import BeautifulSoup
+import htmlmin
+import html
+
+import re
+import sys
+import pymongo
+import requests
+import warnings
+from bs4 import BeautifulSoup
+# from flask import Flask, request as flask_req,jsonify
+
+
+warnings.filterwarnings("ignore")
+# app = Flask(__name__)
+
+
+def connect_mongodb():
+	return pymongo.MongoClient("mongodb://sakoju:E107112358@178.128.98.252:2017/milo-staging?authSource=admin")
+
+def get_dom():
+    mydb = connect_mongodb()
+    dom = mydb["milo-staging"].raws.find_one(sort=[('id',pymongo.DESCENDING)])
+    # print("=========",dom)
+    return (dom)
+
+def getNewsKompasN():
+    dom = get_dom()
+    divnews = dom.find_all("div",{"class":"read__content"})
+    title = dom.find("h1",{"class":"read__title"}).get_text()
+    date = dom.find("div",{"class":"read__time"}).get_text()
+    author = dom.find("div",{"class":"read__author"}).get_text()
+    news = ''
+    for i in divnews:
+        news = news + i.get_text()
+
+    result = {
+        "Title": title,
+        "date": date,
+        "author": author,
+        "News": news
+    }
+
+    return (result)
+
+def getnewsKompas(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    # print(soup)
+    prety = soup.prettify()
+    divnews = soup.find_all("div",{"class":"read__content"})
+    title = soup.find("h1", {"class": "read__title"}).get_text()
+    date = soup.find("div", {"class": "read__time"}).get_text()
+    author = soup.find("div", {"class": "read__author"}).get_text()
+    news = ''
+    for lol in divnews:
+        news = news+lol.get_text()
+    result = {
+        "Title": title,
+        "date": date,
+        "author": author,
+        "News": news
+    }
+    return (result)
+
+def getNewsKumparan(dom):
+    divnews = dom.find_all("div",{"class":"editor-padding-8"})
+    title = ''
+    date = ''
+    author = ''
+    news = ''
+    for i in divnews:
+        news = news+i.get_text()
+
+    result = {
+        "Title": title,
+        "date": date,
+        "author": author,
+        "News": news
+    }
+
+    return (result)
+
+def getNewsOKEZone(dom):
+    divnews = ''
+    title = ''
+    date = ''
+    author = ''
+    news = ''
+
+    result = {
+        "Title": title,
+        "date": date,
+        "author": author,
+        "News": news
+    }
+
+    return (result)
+
+
+def cleanDOM(dom):
+    clean = htmlmin.minify(dom)
+    return(clean)
+
+
+# a = getnewsKompas('https://nasional.kompas.com/read/2018/12/05/16252441/dengan-suara-meninggi-prabowo-cibir-media-massa-soal-jumlah-peserta-reuni')
+# #
+# print(a)
+b = get_dom()
+print(b)
+c = getNewsKompasN()
+print(c)
+
+# print(getnewsKompas(a))
+
+# Flask methods below:
+# @app.route("/scrap", methods=["POST"])
+# def scrap():
+#     dom = flask_req.form.get("dom")
+# 	# source_name = flask_req.form.get("source_name")
+# 	# url = flask_req.form.get("url")
+# 	# url_target = flask_req.form.get("url_target")
+#     source =''
+#     if (source == 'Kumparan'):
+#         scr = getNewsKumparan(dom)
+#     if (source == 'Kompas'):
+#         scr = getnewsKompas(dom)
+#     if (source == 'OKEZone'):
+#         scr = getNewsOKEZone(dom)
+#
+#     return jsonify(scr)
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    print('sumtin wong')
