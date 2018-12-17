14321829359cf13b78a47411eec422c5f70491aa
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-12-17 16:33:50 +0700

    update mongo isntance ca;;

--- src/utils/mongodb.py
+++ src/utils/mongodb.py
@@ -1,5 +1,5 @@
 import pymongo, os
 
 def getInstance():
-    connection =  pymongo.MongoClient(os.getenv('MONGO_CONNECTION_STRING'))
-    return connection["milo-staging"]
+    connection = pymongo.MongoClient(os.getenv('MONGO_CONNECTION_STRING'))
+    return connection["milo-" + str(os.getenv('APP_ENV'))]
