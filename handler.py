0033ec416466844248174dee425cfa54a72f4c94
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-12-22 22:00:06 +0700

    update

--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
+import os
 from src.methods.crawler import do_crawl as crawl
 from src.utils.helpers import response
+from src.transformer.input_transformer import normalize
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -7,10 +9,19 @@ from dotenv import load_dotenv
 load_dotenv(join(dirname(__file__), '.env'))
 
 def crawler_handler(event, context):
-    # TODO: validate input
-    # TODO: map input to function dynamically
+    try:
+        if event['headers']['secret'] != os.getenv('API_SECRET'):
+            return response('not authorized', status=401)
 
-    crawl("detik", "detik.com", "https://detik.com")
-    return response('crawl completed')
+        # get request input
+        [source, url, target_url] = normalize(event)
+
+        # crawl target url
+        crawl(source, url, target_url)
+
+        return response('crawl completed')
+    except:
+        return response('an error occurred', status=500)
+
 
 
