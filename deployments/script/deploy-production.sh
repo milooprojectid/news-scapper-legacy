c887994b4babdf0f9d21a65d4d7d45eb89c89e66
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-04-03 20:44:44 +0700

    update all script to bash

--- deployments/script/deploy-production.sh
+++ deployments/script/deploy-production.sh
@@ -7,9 +7,9 @@ export env
 export script_dir
 
 echo -e "\n==================== Packaging files ====================\n"
-sh $(pwd)/deployments/script/pack.sh
+bash $(pwd)/deployments/script/pack.sh
 
 echo -e "\n==================== Deploying to ${env} ====================\n"
-sh $(pwd)/deployments/script/update-lambda.sh
+bash $(pwd)/deployments/script/update-lambda.sh
 
 echo -e "\n${env} deployed !\n"
