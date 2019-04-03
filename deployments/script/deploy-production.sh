a1b0f9fe76312ca32a6609a183975df1006228a5
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-04-03 20:20:03 +0700

    update script

--- deployments/script/deploy-production.sh
+++ deployments/script/deploy-production.sh
@@ -7,7 +7,7 @@ export env
 export script_dir
 
 echo -e "\n==================== Packaging files ====================\n"
-sh $(pwd)/deployments/script/pack-venv.sh
+sh $(pwd)/deployments/script/pack.sh
 
 echo -e "\n==================== Deploying to ${env} ====================\n"
 sh $(pwd)/deployments/script/update-lambda.sh
